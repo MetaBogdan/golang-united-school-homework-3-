e3a2181414edb950247428e2b6415bbcfc4f4332
Bogdan Vozniy <Baglandir@gmail.com>
2022-05-06 10:44:58 +0800

    2 try

--- task02-slice.go
+++ task02-slice.go
@@ -3,12 +3,12 @@ package homework
 func reverse(input []int64) (result []int64) {
 
 	lenSlice := len(input)
-	result := make([]int64, lenSlice)
+	resultNew := make([]int64, lenSlice)
 
 	for i := lenSlice/2 - 1; i >= 0; i-- {
 		opp := lenSlice - 1 - i
-		result[i], result[opp] = input[opp], input[i]
+		resultNew[i], resultNew[opp] = input[opp], input[i]
 
 	}
-	return result
+	return resultNew
 }
